7978080fbddcebc6ec7ab0b4c8b3418ce42b5fc5
joaomiguelvieira <joaomiguelvieira@live.com.pt>
2019-12-19 13:26:25 +0000

    Fixed .gitignore

--- MCTS_TicTacToe_c/main.c
+++ MCTS_TicTacToe_c/main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+int givenEmptyBoard_whenSimulateInterAIPlay_thenGameDraw() {
+    return 0;
+}
+
+int givenEmptyBoard_whenLevel1VsLevel3_thenLevel3WinsOrDraw () {
+    return 0;
+}
+
 int main() {
     printf("Hello, World!\n");
     return 0;
