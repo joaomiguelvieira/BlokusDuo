a0ad6ad7c2a954a153ad79cad21f3af5f9b31aae
joaomiguelvieira <joaomiguelvieira@live.com.py>
2019-12-25 14:28:06 +0000

    Developing

--- MCTS_BlokusDuo_cpp/main.cpp
+++ MCTS_BlokusDuo_cpp/main.cpp
@@ -4,13 +4,7 @@
 #include "Player.h"
 
 int main() {
-    auto player1 = new Player();
 
-    std::cout << "Player " << player1->getPlayerId() << " has " << player1->getRemainingGamePieces()->size() << " remaining game pieces:\n";
-    for (auto & gamePiece : *player1->getRemainingGamePieces())
-        std::cout << (*gamePiece)[0]->getCodeName() << " has " << gamePiece->size() << " variant(s)" << "\n";
-
-    delete player1;
 
     return 0;
 }
