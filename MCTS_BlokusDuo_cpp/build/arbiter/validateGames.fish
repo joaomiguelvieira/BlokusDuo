5fa13c4d9f2adb12080046346ecaebb5d4bb185e
joaomiguelvieira <joaomiguelvieira@live.com.pt>
2020-01-15 14:59:15 +0000

    Debugging: patched initial set of game pieces in attempt to solve differential move calcultaion

--- MCTS_BlokusDuo_cpp/build/arbiter/validateGames.fish
+++ MCTS_BlokusDuo_cpp/build/arbiter/validateGames.fish
@@ -1,22 +1,67 @@
 #!/bin/fish
 
+if test (count $argv) -ge 1
+  set logFolder $argv[1]
+else
+  set logFolder game_logs
+end
+
 set testsPassed 0
 set testsFailed 0
 set player1Wins 0
 set player2Wins 0
-set gameDraw 0
+set gameDraws 0
 
-for i in (seq 1 100)
-	set result (./blokus-host < game_logs4/game$i.txt | grep "Both pass!")
+for gameLog in $logFolder/*.txt
+	set result (./blokus-host < $gameLog | grep "Both pass!")
+  set winner (./blokus-host < $gameLog | grep "Tiles:")
 
 	if string match -q "Both pass!" -- "$result"
+    echo "INFO: test $gameLog passed!" 
 		set testsPassed (math $testsPassed + 1)
+
+    if string match -q "*Player 1*" -- "$winner"
+      set player1Wins (math $player1Wins + 1)
+    else if string match -q "*Player 2*" -- "$winner"
+      set player2Wins (math $player2Wins + 1)
+    else
+      set gameDraws (math $gameDraws + 1)
+    end
 	else
-		echo "TEST $i FAILED!"
+		echo "ERROR: test $gameLog failed!"
 		set testsFailed (math $testsFailed + 1)
 	end
 end
 
 set totalTests (math $testsPassed + $testsFailed)
 
-echo "PASSED $testsPassed/$totalTests"
+set differentGames
+
+set numberOfGames (count $logFolder/*.txt)
+
+for i in (seq 1 $numberOfGames)
+  set original 1
+
+  for j in (seq $i $numberOfGames)
+    if test "$i" -ne "$j"
+      if string match -q (diff $gameLogs/game$i.txt $gameLogs/game$j.txt) -- ""
+        set original 0
+        break
+      end
+    end
+  end
+
+  if test "$original" -eq "1"
+    set differentGames (math $differentGames + 1)
+  end
+end
+
+
+echo "========== SUMMARY =========="
+echo "Different games: $differentGames"
+echo "-----------------------------"
+echo "Player 1 victories: $player1Wins"
+echo "Player 2 victories: $player2Wins"
+echo "Game draws: $gameDraws"
+echo "-----------------------------"
+echo "Tests passed $testsPassed/$totalTests"
